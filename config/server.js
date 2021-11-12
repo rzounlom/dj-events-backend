d94fce3950cd350b26656587b66951112e9663f6
Romaric Zounlome <rzounlom@gmail.com>
2021-11-12 10:44:29 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '800f826641da72cf977b3a72fce6d424'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9d78398a-9f56-42cf-9600-1d86bad8a448'
+};
